70d08b22809b2e71e29d3d8d63bfcd609e1977d2
masterUNG <phrombutr@gmail.com>
2016-12-08 23:26:17 +0700

    Image 0 ok

--- app/src/main/java/rtc/papatsara/kanyanee/amazingsuanphueng/Detail2Activity.java
+++ app/src/main/java/rtc/papatsara/kanyanee/amazingsuanphueng/Detail2Activity.java
@@ -1,22 +1,13 @@
 package rtc.papatsara.kanyanee.amazingsuanphueng;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.TextView;
+import com.squareup.picasso.Picasso;
 
 public class Detail2Activity extends AppCompatActivity {
 
@@ -24,7 +15,7 @@ public class Detail2Activity extends AppCompatActivity {
     private TextView titleTextView, detailTextView;
     private ImageView imageView;
     private Button button;
-
+    private String image1String, image2String, image3String, image4String;
 
 
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,12 +28,19 @@ public class Detail2Activity extends AppCompatActivity {
         imageView = (ImageView) findViewById(R.id.imageView2);
         button = (Button) findViewById(R.id.button2);
 
+
+
+        image1String = getIntent().getStringExtra("Image1");
+        image2String = getIntent().getStringExtra("Image2");
+        image3String = getIntent().getStringExtra("Image3");
+        image4String = getIntent().getStringExtra("Image4");
+
+
         // Show View
         titleTextView.setText(getIntent().getStringExtra("Title"));
         detailTextView.setText(getIntent().getStringExtra("Detail"));
 
-
-
+        showImage(0);
 
 
         // Button Controller
@@ -55,5 +53,13 @@ public class Detail2Activity extends AppCompatActivity {
 
 
     }   // Main Method
+
+    private void showImage(int index) {
+
+        String[] strings = new String[]{image1String, image2String, image3String, image4String};
+        Picasso.with(Detail2Activity.this).load(strings[index]).into(imageView);
+
+    }
+
 
 } // Main Class
